af279bcff18c71bfa4aaea136719d2f268389949
anton <demeshkevichanton@gmail.com>
2022-03-19 23:30:13 +0300

    ref

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <button @click="addLike">like</button>
+    <button @click="addDislike">dislike</button>
+    <div>
+      количество лайков:<strong>{{ likes }}</strong>
+    </div>
+    <div>
+      количество дизлайков:<strong>{{ dislikes }}</strong>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      likes: 0,
+      dislikes: 10,
+    };
+  },
+  methods: {
+    addLike() {
+      this.likes += 1;
+    },
+    addDislike() {
+      this.dislikes += 1;
+    },
+  },
+};
+</script>
+<style>
+</style>
