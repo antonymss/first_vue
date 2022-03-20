1cd3992fdbaeb2a10a056ce308108899ec5ceca5
anton <demeshkevichanton@gmail.com>
2022-03-20 13:10:40 +0300

    refact

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,86 @@
 <template>
-  <div>
-    <button @click="addLike">like</button>
-    <button @click="addDislike">dislike</button>
-    <div>
-      количество лайков:<strong>{{ likes }}</strong>
-    </div>
-    <div>
-      количество дизлайков:<strong>{{ dislikes }}</strong>
+  <div class="app">
+    <form action="">
+      <h4>Создание поста</h4>
+      <input
+        v-bind:value="title"
+        @input="title = $event.target.value"
+        class="input"
+        type="text"
+        placeholder="Название "
+      />
+      <input
+        v-bind:value="body"
+        @input="body =$event.target.value"
+        class="input"
+        type="text"
+        placeholder="Описание"
+      />
+      <button class="btn" @click="createPost">Создание поста</button>
+    </form>
+    <div class="post" v-for="post in posts">
+      <div><strong>Название:</strong> {{ post.title }}</div>
+      <div><strong>Описание:</strong> {{ post.body }}</div>
     </div>
   </div>
 </template>
+
 <script>
 export default {
   data() {
     return {
-      likes: 0,
-      dislikes: 10,
+      posts: [
+        { id: 1, title: "JS", body: "description" },
+        { id: 2, title: "JS 2", body: "description" },
+        { id: 3, title: "JS 3", body: "description" },
+      ],
+      title: "",
+      body: "",
     };
   },
   methods: {
-    addLike() {
-      this.likes += 1;
+    createPost() {
+      const newPost = {
+        id: Date.now(),
+        title: this.title,
+        body: this.body,
+      }
+      this.posts.push(newPost)
     },
-    addDislike() {
-      this.dislikes += 1;
-    },
+    
   },
 };
 </script>
 <style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+.app {
+  padding: 20px;
+}
+.post {
+  padding: 15px;
+  border: 2px solid teal;
+  margin-top: 15px;
+}
+form {
+  display: flex;
+  flex-direction: column;
+}
+.input {
+  width: 100%;
+  border: 1px solid teal;
+  padding: 10px 15px;
+  margin-top: 15px;
+}
+.btn {
+  align-self: flex-end;
+  margin-top: 15px;
+  padding: 10px 15px;
+  background: none;
+  color: teal;
+  border: 1px solid teal;
+}
 </style>
