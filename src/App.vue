85abd836ebb528b840ad0edcef8c8bbee997f376
anton <demeshkevichanton@gmail.com>
2022-03-20 13:54:35 +0300

    ref

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,18 @@
 <template>
   <div class="app">
-    <form @submit.prevent>
-      <h4>Создание поста</h4>
-      <input
-        v-bind:value="title"
-        @input="title = $event.target.value"
-        class="input"
-        type="text"
-        placeholder="Название "
-      />
-      <input
-        v-bind:value="body"
-        @input="body = $event.target.value"
-        class="input"
-        type="text"
-        placeholder="Описание"
-      />
-      <button class="btn" @click="createPost">Создание поста</button>
-    </form>
-    <div class="post" v-for="post in posts">
-      <div><strong>Название:</strong> {{ post.title }}</div>
-      <div><strong>Описание:</strong> {{ post.body }}</div>
-    </div>
+    <post-form/>
+    <post-list :posts="posts"/>
   </div>
 </template>
 
 <script>
+import PostForm from "@/components/PostForm.vue"
+import PostList from "@/components/PostList.vue"
+
 export default {
+  components:{
+    PostList, PostForm
+  },
   data() {
     return {
       posts: [
@@ -34,21 +20,12 @@ export default {
         { id: 2, title: "JS 2", body: "description" },
         { id: 3, title: "JS 3", body: "description" },
       ],
-      title: "",
-      body: "",
+      
     };
   },
   methods: {
     createPost() {
-      const newPost = {
-        id: Date.now(),
-        title: this.title,
-        body: this.body,
-      };
-      this.posts.push(newPost);
-      this.title = "";
-      this.body = "";
-    },
+     
   },
 };
 </script>
@@ -61,27 +38,6 @@ export default {
 .app {
   padding: 20px;
 }
-.post {
-  padding: 15px;
-  border: 2px solid teal;
-  margin-top: 15px;
-}
-form {
-  display: flex;
-  flex-direction: column;
-}
-.input {
-  width: 100%;
-  border: 1px solid teal;
-  padding: 10px 15px;
-  margin-top: 15px;
-}
-.btn {
-  align-self: flex-end;
-  margin-top: 15px;
-  padding: 10px 15px;
-  background: none;
-  color: teal;
-  border: 1px solid teal;
-}
+
+
 </style>
