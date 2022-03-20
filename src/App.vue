2b3a780f46d1561fac91997c2ea3feb726518fbd
anton <demeshkevichanton@gmail.com>
2022-03-21 00:06:32 +0300

    ref

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="app">
     <h1>Страница с постами</h1>
-    <my-button @click="showDialog">Создать пост</my-button>
+    <my-button @click="fetchPosts">получить посты</my-button>
+    <my-button @click="showDialog" style="margin: 15px 0">
+      создать пост
+    </my-button>
     <my-dialog v-model:show="dialogVisible">
       <post-form @create="createPost" />
     </my-dialog>
@@ -13,9 +16,9 @@
 <script>
 import PostForm from "@/components/PostForm.vue";
 import PostList from "@/components/PostList.vue";
-import MyDialog from "./components/UI/MyDialog.vue";
-import MyButton from "./UI/MyButton.vue";
-// import MyDialog from "./UI/MyDialog.vue";
+import MyDialog from "./components/UI/MyDialog";
+import MyButton from "./components/UI/MyButton";
+import axios from "axios";
 
 export default {
   components: {
@@ -23,27 +26,34 @@ export default {
     PostForm,
     MyDialog,
     MyButton,
+    MyButton,
   },
   data() {
     return {
-      posts: [
-        { id: 1, title: "JS", body: "description" },
-        { id: 2, title: "JS 2", body: "description" },
-        { id: 3, title: "JS 3", body: "description" },
-      ],
+      posts: [],
       dialogVisible: false,
     };
   },
   methods: {
     createPost(post) {
       this.posts.push(post);
-      this.dialogVisible = false
+      this.dialogVisible = false;
     },
     removePost(post) {
       this.posts = this.posts.filter((p) => p.id !== post.id);
     },
     showDialog() {
       this.dialogVisible = true;
+    },
+    async fetchPosts() {
+      try {
+        const response = await axios.get(
+          "https://jsonplaceholder.typicode.com/posts?_limit=10"
+        );
+        this.posts = response.data;
+      } catch (e) {
+        alert("error");
+      }
     },
   },
 };
